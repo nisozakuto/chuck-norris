30252a9b9271ae12d00a2628d5ad8933132cdb71
nisozakuto <nisozakuto@gmail.com>
2020-12-07 22:09:51 -0500

    version 0.1 w/o styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      randomResponse: "",
+    };
+  }
+
+  connectToChuck = () => {
+    fetch(
+      "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random",
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          "x-rapidapi-key": process.env.REACT_APP_KEY,
+          "x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          randomResponse: res,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.connectToChuck();
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Chuck Norris Jokes</h1>
+        <form className="jokesForm" onSubmit={this.handleSubmit}>
+          <label>Get a new joke</label>
+          <input type="submit" value="Go!"></input>
+        </form>
+        {this.state.randomResponse ? (
+          <p>{this.state.randomResponse.value}</p>
+        ) : (
+          <p>Make a search</p>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
